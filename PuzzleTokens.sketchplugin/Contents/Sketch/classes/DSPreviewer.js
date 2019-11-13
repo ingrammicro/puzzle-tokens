3985891801bde0dbdf2f99094dd41e0bd2552ca3
Max Bazarov <max@bazarov.ru>
2019-11-13 18:16:42 +0300

    Working on Styles Overview

--- PuzzleTokens.sketchplugin/Contents/Sketch/classes/DSPreviewer.js
+++ PuzzleTokens.sketchplugin/Contents/Sketch/classes/DSPreviewer.js
@@ -26,8 +26,61 @@ class DSPreviewer {
 
         this.errors = []
 
+        this._init()
         // init global variable
         app = this
+    }
+
+    _init() {
+        this.defGroup = {
+            labelColor: "#353536",
+            labelFontSize: 25,
+            bottomSpace: 20,
+        }
+        this.defText = {
+            initialTop: 25,
+            initialLeft: 25,
+            text: "Aa",
+            columns: 9,
+            colWidth: 100,
+            colVSpace: 50,
+            colHSpace: 50,
+            textTop: 5,
+            textBottom: 5,
+            labelFontFamily: "Helvetica",
+            labelFontWeight: 5,
+            labelColor: "#353536",
+            labelFontSize: 20,
+            descrFontSize: 12,
+            descrColor: "#B1B1B1",
+            pageWidth: 1600,
+            pageHeight: 1200,
+        }
+
+        this.groupLabelStyle = {
+            textColor: this.defGroup.labelColor,
+            alignment: Text.Alignment.left,
+            fontSize: this.defGroup.labelFontSize,
+            lineHeight: this.defGroup.labelFontSize * 1.5,
+        }
+
+        this.labelStyle = {
+            textColor: this.defText.labelColor,
+            alignment: Text.Alignment.left,
+            fontSize: this.defText.labelFontSize,
+            lineHeight: 20,
+            fontFamily: this.defText.labelFontFamily,
+            fontWeight: this.defText.labelFontWeight,
+        }
+
+        this.descrStyle = {
+            textColor: this.defText.descrColor,
+            alignment: Text.Alignment.left,
+            fontSize: this.defText.descrFontSize,
+            lineHeight: this.defText.descrFontSize * 1.5,
+            fontFamily: this.defText.labelFontFamily,
+            fontWeight: this.defText.labelFontWeight,
+        }
     }
 
     // Tools
@@ -126,7 +179,8 @@ class DSPreviewer {
             name: 'Overview',
             parent: this.sPage,
             frame: new Rectangle(
-                0, 0, 1000, 1000
+                0, 0, this.defText.pageWidth, this.defText.pageHeight
+
             )
         })
 
@@ -145,35 +199,70 @@ class DSPreviewer {
     }
 
     _showTextStyles() {
-        var y = 25
-        const offsetX = 25
-        const textExample = "Aa"
-        const colLimit = 5
-        const colWidth = 100
-        const colSpace = 50
-        const textOffsetTop = 5
-        const textOffsetBottom = 5
-        let colIndex = 1
+        const sharedTextStyles = this.sDoc.sharedTextStyles
+        // build style group list
+        var styleGroups = {}
+        sharedTextStyles.forEach(function (sSharedStyle, styleIndex) {
+            let groupName = "top"
+            const lastShashIndex = sSharedStyle.name.lastIndexOf("/")
+            if (lastShashIndex > 0) {
+                groupName = sSharedStyle.name.substring(0, lastShashIndex)
+            }
+            let group = styleGroups[groupName]
+            if (undefined == group) {
+                group = []
+                styleGroups[groupName] = group
+            }
+            group.push(sSharedStyle)
+        }, this)
 
-        const labelStyle = {
-            textColor: "#353536",
-            alignment: Text.Alignment.left,
-            fontSize: 12
+        // show groups
+        var y = this.defText.initialTop
+        for (let [groupName, styles] of Object.entries(styleGroups)) {
+            y = this._showTextStyleGroupLabel(groupName, y)
+            y = this._showTextStyleGroup(styles, y)
         }
+    }
 
+    _showTextStyleGroupLabel(groupName, y) {
+        groupName = groupName.replace(/[/]/g, " / ")
+        const sText = new Text({
+            name: "Group Label",
+            text: groupName,
+            parent: this.sArtboard,
+            frame: new Rectangle(
+                this.defText.initialLeft, y, 400, this.groupLabelStyle.lineHeight
+            ),
+            style: this.groupLabelStyle,
+        })
+        return y + this.groupLabelStyle.lineHeight + this.defGroup.bottomSpace
+    }
+
+    _showTextStyleGroup(styles, y) {
+
+        const offsetX = this.defText.initialLeft
+
+        const textExample = this.defText.text
+        const colLimit = this.defText.columns
+        const colWidth = this.defText.colWidth
+        const colHSpace = this.defText.colHSpace
+        const colVSpace = this.defText.colVSpace
+        const textOffsetTop = this.defText.textTop
+        const textOffsetBottom = this.defText.textBottom
+        let colIndex = 1
         let x = offsetX
-        let colHeight = 100
+        let colHeight = 0
 
-        const textSharedStyles = this.sDoc.sharedTextStyles
-        textSharedStyles.forEach(function (sSharedStyle, styleIndex) {
+
+        styles.forEach(function (sSharedStyle, styleIndex) {
 
             /// calculate max height of texts in this row
             if (1 == colIndex) {
                 colHeight = 0
-                let last = Math.min(styleIndex + colLimit - 1, textSharedStyles.length - 1)
+                let last = Math.min(styleIndex + colLimit - 1, styles.length - 1)
                 for (let index = styleIndex; index <= last; index++) {
-                    const sStyle = textSharedStyles[index].style
-                    colHeight = Math.max(colHeight, sStyle.fontSize)
+                    const sStyle = styles[index].style
+                    colHeight = Math.max(colHeight, sStyle.lineHeight)
                 }
                 colHeight += textOffsetTop + textOffsetBottom
             }
@@ -193,6 +282,14 @@ class DSPreviewer {
                 borders: [{ color: '#979797' }],
             }
 
+            let styleName = sSharedStyle.name
+            const lastShashIndex = styleName.lastIndexOf("/")
+            if (lastShashIndex > 0) {
+                styleName = styleName.substring(lastShashIndex + 1)
+            }
+
+            let descr = sStyle.fontFamily + " " + sStyle.fontSize + "px"
+
             ///
 
             const sParent = new Group({
@@ -202,7 +299,7 @@ class DSPreviewer {
                     x, y, width, height
                 )
             })
-            //const sParent = this.sArtboard
+            let localY = 0
 
 
             const sBack = new Shape({
@@ -210,48 +307,64 @@ class DSPreviewer {
                 parent: sParent,
                 style: backStyle,
                 frame: new Rectangle(
-                    0, 0, width, height
+                    0, localY, width, height
                 )
             })
+            localY += textOffsetTop
 
             const sText = new Text({
                 name: "Text",
                 text: textExample,
                 parent: sParent,
                 frame: new Rectangle(
-                    0 + 5, 0 + textOffsetTop, width, sStyle.fontSize
+                    0 + 5, 0 + localY, width, sStyle.lineHeight
                 ),
                 style: sStyle,
                 sharedStyleId: sSharedStyle.id
             })
-            //sText.adjustToFit()
+            localY += colHeight
 
             const sLabel = new Text({
                 name: "Label",
-                text: sSharedStyle.name,
+                text: styleName,
                 parent: sParent,
                 frame: new Rectangle(
-                    0, y + colHeight, width, labelStyle.fontSize
+                    0, localY, width, this.labelStyle.lineHeight
                 ),
-                style: labelStyle
+                style: this.labelStyle
             })
+            localY += this.labelStyle.lineHeight
+
+            const sDescr = new Text({
+                name: "Description",
+                text: descr,
+                parent: sParent,
+                frame: new Rectangle(
+                    0, localY, width, this.descrStyle.lineHeight
+                ),
+                style: this.descrStyle
+            })
+            localY += this.descrStyle.lineHeight
 
 
             // Final adjustment
             //sGroup.adjustToFit()
 
             // Calculate next cell position
-            if (colIndex % colLimit === 0) {
+            if (colIndex % colLimit === 0 || styleIndex == (styles.length - 1)) {
                 // make new row
-                y += colHeight + labelStyle.fontSize
+                y += localY + colVSpace
                 x = offsetX
                 colIndex = 1
+                log("next row, y=" + y + " colHeight=" + colHeight + " sLabel.frame.height=" + sLabel.frame.height + " colVSpace=" + colVSpace)
             } else {
                 // add new column
                 colIndex++
-                x += colWidth + colSpace
+                x += colWidth + colHSpace
             }
         }, this)
+
+        return y
     }
 
     ///////////////////////////////////////////////////////////////
