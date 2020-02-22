e110081b9ca40ff03c49ea9de719c171e0d755ab
Maxim Bazarov <baza@odin.com>
2020-02-22 23:19:54 +0300

    Added support for HSLA and HSL colors

--- PuzzleTokens.sketchplugin/Contents/Sketch/lib/utils.js
+++ PuzzleTokens.sketchplugin/Contents/Sketch/lib/utils.js
@@ -209,6 +209,138 @@ class Utils {
         return '#' + hex.match(/[a-f0-9]{2}/ig).map(e => (255 - parseInt(e, 16) | 0).toString(16).replace(/^([a-f0-9])$/, '0$1')).join('')
     }
 
+    // source: https://codepen.io/jkantner/pen/VVEMRK    
+    static HSLAToHexA(hsla) {
+        let ex = /^hsla\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)(((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2},\s?)|((\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}\s\/\s))((0?\.\d+)|[01]|(([1-9]?\d(\.\d+)?)|100|(\.\d+))%)\)$/i;
+        if (ex.test(hsla)) {
+            let sep = hsla.indexOf(",") > -1 ? "," : " ";
+            hsla = hsla.substr(5).split(")")[0].split(sep);
+
+            // strip the slash
+            if (hsla.indexOf("/") > -1)
+                hsla.splice(3, 1);
+
+            let h = hsla[0],
+                s = hsla[1].substr(0, hsla[1].length - 1) / 100,
+                l = hsla[2].substr(0, hsla[2].length - 1) / 100,
+                a = hsla[3];
+
+            // strip label and convert to degrees (if necessary)
+            if (h.indexOf("deg") > -1)
+                h = h.substr(0, h.length - 3);
+            else if (h.indexOf("rad") > -1)
+                h = Math.round(h.substr(0, h.length - 3) * (180 / Math.PI));
+            else if (h.indexOf("turn") > -1)
+                h = Math.round(h.substr(0, h.length - 4) * 360);
+            if (h >= 360)
+                h %= 360;
+
+            // strip % from alpha, make fraction of 1 (if necessary)
+            if (a.indexOf("%") > -1)
+                a = a.substr(0, a.length - 1) / 100;
+
+            let c = (1 - Math.abs(2 * l - 1)) * s,
+                x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+                m = l - c / 2,
+                r = 0,
+                g = 0,
+                b = 0;
+
+            if (0 <= h && h < 60) {
+                r = c; g = x; b = 0;
+            } else if (60 <= h && h < 120) {
+                r = x; g = c; b = 0;
+            } else if (120 <= h && h < 180) {
+                r = 0; g = c; b = x;
+            } else if (180 <= h && h < 240) {
+                r = 0; g = x; b = c;
+            } else if (240 <= h && h < 300) {
+                r = x; g = 0; b = c;
+            } else if (300 <= h && h < 360) {
+                r = c; g = 0; b = x;
+            }
+            r = Math.round((r + m) * 255).toString(16);
+            g = Math.round((g + m) * 255).toString(16);
+            b = Math.round((b + m) * 255).toString(16);
+            a = Math.round(a * 255).toString(16);
+
+            if (r.length == 1)
+                r = "0" + r;
+            if (g.length == 1)
+                g = "0" + g;
+            if (b.length == 1)
+                b = "0" + b;
+            if (a.length == 1)
+                a = "0" + a;
+
+            return "#" + r + g + b + a;
+        } else {
+            app.logError("HSLAToHexA() Invalid input color: " + hsla)
+            return "#000000FF"
+        }
+    }
+
+    // source: https://codepen.io/jkantner/pen/VVEMRK    
+    static HSLToHex(hsl) {
+        let ex = /^hsl\(((((([12]?[1-9]?\d)|[12]0\d|(3[0-5]\d))(\.\d+)?)|(\.\d+))(deg)?|(0|0?\.\d+)turn|(([0-6](\.\d+)?)|(\.\d+))rad)((,\s?(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2}|(\s(([1-9]?\d(\.\d+)?)|100|(\.\d+))%){2})\)$/i;
+        if (ex.test(hsl)) {
+            let sep = hsl.indexOf(",") > -1 ? "," : " ";
+            hsl = hsl.substr(4).split(")")[0].split(sep);
+
+            let h = hsl[0],
+                s = hsl[1].substr(0, hsl[1].length - 1) / 100,
+                l = hsl[2].substr(0, hsl[2].length - 1) / 100;
+
+            // strip label and convert to degrees (if necessary)
+            if (h.indexOf("deg") > -1)
+                h = h.substr(0, h.length - 3);
+            else if (h.indexOf("rad") > -1)
+                h = Math.round(h.substr(0, h.length - 3) * (180 / Math.PI));
+            else if (h.indexOf("turn") > -1)
+                h = Math.round(h.substr(0, h.length - 4) * 360);
+            if (h >= 360)
+                h %= 360;
+
+            let c = (1 - Math.abs(2 * l - 1)) * s,
+                x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+                m = l - c / 2,
+                r = 0,
+                g = 0,
+                b = 0;
+
+            if (0 <= h && h < 60) {
+                r = c; g = x; b = 0;
+            } else if (60 <= h && h < 120) {
+                r = x; g = c; b = 0;
+            } else if (120 <= h && h < 180) {
+                r = 0; g = c; b = x;
+            } else if (180 <= h && h < 240) {
+                r = 0; g = x; b = c;
+            } else if (240 <= h && h < 300) {
+                r = x; g = 0; b = c;
+            } else if (300 <= h && h < 360) {
+                r = c; g = 0; b = x;
+            }
+            // having obtained RGB, convert channels to hex
+            r = Math.round((r + m) * 255).toString(16);
+            g = Math.round((g + m) * 255).toString(16);
+            b = Math.round((b + m) * 255).toString(16);
+
+            // prepend 0s if necessary
+            if (r.length == 1)
+                r = "0" + r;
+            if (g.length == 1)
+                g = "0" + g;
+            if (b.length == 1)
+                b = "0" + b;
+
+            return "#" + r + g + b;
+
+        } else {
+            app.logError("HSLToHex() Invalid input color: " + hsl)
+            return "#000000"
+        }
+    }
 
     // str: white or #32333 or #12345678 or #112233 %10
     // opacity: see opacityToHex()
@@ -216,6 +348,11 @@ class Utils {
         // process #112233 %10
         if (str.includes("transparent")) {
             return "#FFFFFF" + Utils.opacityToHex(0)
+        }
+        if (str.toLowerCase().includes("hsla")) {
+            str = Utils.HSLAToHexA(str)
+        } else if (str.toLowerCase().includes("hsl")) {
+            str = Utils.HSLToHex(str)
         }
         if (str.includes(" ") && str.includes("%")) {
             const list = str.split(" ")
