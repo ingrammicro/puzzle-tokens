40f2a898490a804c7ca163823d55d7decf860bc1
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2020-03-17 22:40:58 +0300

    Create SASS file with tokens

--- PuzzleTokens.sketchplugin/Contents/Sketch/constants.js
+++ PuzzleTokens.sketchplugin/Contents/Sketch/constants.js
@@ -8,6 +8,7 @@ var Constants = {
     SYMBOLTOKENFILE_POSTFIX: "-inspector.json",
     CSSFILE_POSTFIX: "-viewer.css",
     VARSFILE_POSTFIX: "-vars.json",
+    SASSFILE_POSTFIX: "-vars.scss",
     SYMBOLPAGE_NAME: "Symbols",
 
     SITE_CHANGELOG_URL: "https://github.com/ingrammicro/puzzle-tokens/blob/master/CHANGELOG.md",
@@ -17,7 +18,6 @@ var SettingKeys = {
     PLUGIN_PREVIEWER_DEF: "pluginPreviewerDef",
     PLUGIN_PATH_TO_TOKENS_LESS: "pluginPathToTokensLess",
     PLUGIN_PATH_TO_TOKENS_LESS_LIST: "pluginPathToTokensLessList",
-    PLUGIN_PATH_TO_SKETCHSTYLES_LESS: "pluginPathToSketchStylesJSON",
     PLUGIN_GENERATE_SYMBOLTOKENS: "pluginGenSymbTokens",
     PLUGIN_SHOW_CHECK: "pluginPreview",
     PLUGIN_SHOW_DEBUG: "pluginDebug",
