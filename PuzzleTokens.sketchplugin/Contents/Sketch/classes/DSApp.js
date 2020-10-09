3235cb643f8a87342b0c3112e7d2622ca321720e
Josh Clark <josh@bigmedium.com>
2020-10-09 15:27:59 -0400

    Height/width dimensions have to be adjusted first, or bottom-margin and right-margin are incorrect.

--- PuzzleTokens.sketchplugin/Contents/Sketch/classes/DSApp.js
+++ PuzzleTokens.sketchplugin/Contents/Sketch/classes/DSApp.js
@@ -1633,6 +1633,13 @@ class DSApp {
                 let x = null
                 let y = null
 
+                if (null != height) {
+                    l.frame.height = parseInt(height.replace('px', ""))
+                }
+                if (null != width) {
+                    l.frame.width = parseInt(width.replace('px', ""))
+                }
+
                 if (null != margin["top"]) { // prefer top positioning to bottom
                     y = margin["top"] + yOffset;
                 }
@@ -1644,12 +1651,6 @@ class DSApp {
                 }
                 else if (null != margin["right"]) {
                     x = parentFrame.width + xOffset - (margin["right"] + l.frame.width)
-                }
-                if (null != height) {
-                    l.frame.height = parseInt(height.replace('px', ""))
-                }
-                if (null != width) {
-                    l.frame.width = parseInt(width.replace('px', ""))
                 }
 
                 if (x != null || y != null) this.positionInArtboard(l, x, y)
